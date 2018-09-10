0fa902a0f55a6f4955b5149144e24e81b3d26016
ZINAHAL <albaghdz@tcd.ie>
2018-09-10 17:07:34 +0100

    creates a desktop icon in a new created directory

--- runScript.sh
+++ runScript.sh
@@ -3,13 +3,11 @@
 # Create an executable jar file
 mkdir sticky_note 
 javac -d sticky_note RunApplication.java
-cp img.png sticky_note
-cd sticky_note
-echo "Main-Class: RunApplication" > manifest.mf
-jar -cmf manifest.mf runSN.jar *.class
+(cd sticky_note; echo "Main-Class: RunApplication" > manifest.mf; jar -cmf manifest.mf runSN.jar *.class)
 
-# Create .desktop file for the SN application
-# ************* The full path to the new created dir should be placed here
-echo -e "Encoding=UTF-8\nVersion=1.0 \nType=Application\nName=My very Sticky Notes\nTerminal=false" > SNapp.desktop
-echo -e "Exec=java -jar [***********]/sticky_note/runSN.jar\nIcon=[*************]/sticky_note/img.png" >> SNapp.desktop
-chmod +x SNapp.desktop
+# Create .desktop file for the Sticky Note application
+# ****** The full path to the img/directory should be placed here
+(cd sticky_note; echo -e "[Desktop Entry]\nEncoding=UTF-8\nVersion=1.0\nType=Application\nName=My Very Sticky Notes" > stickyNote.desktop;
+echo -e "Exec=java -jar [*******]/sticky_note/runSN.jar\nIcon=[*******]/img.png\nTerminal=false" >> stickyNote.desktop; chmod +x stickyNote.desktop)
+
+# The "Terminal" entry for the .desktop file should always be placed at the end of the file (if you decided to include the entry) 
