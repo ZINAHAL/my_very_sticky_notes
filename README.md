6b85fc23cdd553aef88d672a951a5524e9187d61
ZINAHAL <albaghdz@tcd.ie>
2021-09-01 00:54:18 +0100

    Did script refactoring, no longer requires modifications from the user, can be used straight away

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # My Very Sticky Notes
 
 ![snimg](https://user-images.githubusercontent.com/23452999/45319872-f3ed2080-b538-11e8-9600-f27ffcfa7ac5.png)
+
+1. Developed and tested on Linux os only
+2. HOW TO RUN:
+  1. Clone the repo in a direcotry
+  2. cd to that directory
+  3. "./runScript.sh" in the terminal. The app is found in the newly created directory
--- runScript.sh
+++ runScript.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+source_code_path=$(pwd)
+app_container="app_container"
+
+set -e
+
+mkdir $app_container
+javac -d $app_container RunApplication.java
+cd $app_container
+
 # Create an executable jar file
-mkdir sticky_note 
-javac -d sticky_note RunApplication.java
-(cd sticky_note; echo "Main-Class: RunApplication" > manifest.mf; jar -cmf manifest.mf runSN.jar *.class)
+echo "Main-Class: RunApplication" > manifest.mf
+jar -cmf manifest.mf runSN.jar *.class
 
 # Create .desktop file for the Sticky Note application
-# ****** The full path to the img/directory should be placed here
-(cd sticky_note; echo -e "[Desktop Entry]\nEncoding=UTF-8\nVersion=1.0\nType=Application\nName=My Very Sticky Notes" > stickyNote.desktop;
-echo -e "Exec=java -jar [*******]/sticky_note/runSN.jar\nIcon=[*******]/img.png\nTerminal=false" >> stickyNote.desktop; chmod +x stickyNote.desktop)
+echo -e "[Desktop Entry]\nEncoding=UTF-8\nVersion=1.0\nType=Application\nName=My Very Sticky Notes" > stickyNote.desktop
+echo -e "Exec=java -jar ${source_code_path}/${app_container}/runSN.jar\nIcon=${source_code_path}/img.png\nTerminal=false" >> stickyNote.desktop
+chmod +x stickyNote.desktop
 
 # The "Terminal" entry for the .desktop file should always be placed at the end of the file (if you decided to include the entry) 
